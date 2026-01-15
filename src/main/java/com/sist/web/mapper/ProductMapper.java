b0e36ab4c10cb84e761339c9b9a3cb223dde1366
necteo <89801040+necteo@users.noreply.github.com>
2026-01-15 18:24:17 +0900

    sjh rm selectkey

--- src/main/java/com/sist/web/mapper/ProductMapper.java
+++ src/main/java/com/sist/web/mapper/ProductMapper.java
@@ -16,12 +16,8 @@ public interface ProductMapper {
 		  + "VALUES(seq_item_id.nextval, #{item_name}, #{item_size}, #{item_price}, #{base_item_id})")
 	public void productItemInsert(ProductItemVO vo);
 	
-	@SelectKey(keyProperty = "item_id", resultType = int.class, before = true,
-			statement = "SELECT seq_item_id.currval FROM dual")
-	@SelectKey(keyProperty = "category_id", resultType = int.class, before = true,
-	statement = "SELECT seq_category_id.currval FROM dual")
 	@Insert("INSERT INTO product_item_category(item_category_id, item_id, category_id) "
-		  + "VALUES(seq_item_category_id.nextval, #{item_id}, #{category_id})")
+		  + "VALUES(seq_item_category_id.nextval, seq_item_id.currval, seq_category_id.currval)")
 	public void productItemCategoryInsert(ProductItemCategoryVO vo);
 	
 	@SelectKey(keyProperty = "item_id", resultType = int.class, before = true,
@@ -32,12 +28,8 @@ public interface ProductMapper {
 		  + "#{product_price}, #{discount}, #{description}, #{is_combo})")
 	public void storeProductInsert(StoreProductVO vo);
 
-	@SelectKey(keyProperty = "product_id", resultType = int.class, before = true,
-			statement = "SELECT seq_product_id.currval FROM dual")
-	@SelectKey(keyProperty = "item_id", resultType = int.class, before = true,
-	statement = "SELECT seq_item_id.currval FROM dual")
 	@Insert("INSERT INTO product_combo(combo_id, product_id, item_id, is_upgrade, upgrade_price, item_quantity) "
-		  + "VALUES(seq_combo_id.nextval, #{product_id}, #{item_id}, #{is_upgrade}, #{upgrade_price}, #{item_quantity})")
+		  + "VALUES(seq_combo_id.nextval, seq_product_id.currval, seq_item_id.currval, #{is_upgrade}, #{upgrade_price}, #{item_quantity})")
 	public void productComboInsert(ProductComboVO vo);
 	
 }
